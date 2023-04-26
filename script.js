a6aca920fd009ef1b75899eb6914d408e774b50c
Yakiyo <yakiyosama5855@gmail.com>
2023-04-26 12:50:07 +0600

    feat: script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+const jokeBox = document.getElementById('joke');
+/**
+ * @param {string} cat
+ * @returns string
+ */
+function getUrl(cat) {
+  return `https://v2.jokeapi.dev/joke/${
+    cat.charAt(0).toUpperCase() + cat.substring(1).toLowerCase()
+  }?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single`;
+}
+
+document.addEventListener('click', async (e) => {
+  let { joke } = await fetch(getUrl(e.target.name)).then((v) => v.json());
+  jokeBox.textContent = joke;
+});
